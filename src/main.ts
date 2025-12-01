93723d30c3eca456bc7a5d177afb775878abdab2
SpiderMeow <thoconditonglao123@gmail.com>
2025-12-02 01:28:57 +0700

    Fix đường dẫn #4

--- src/main.ts
+++ src/main.ts
@@ -60,6 +60,7 @@ const corsOptions: cors.CorsOptions = {
       "http://localhost:3000",
       process.env.FRONTEND_URL,
       "https://frontend-fruit-shop-w3b3-i6rmf5pej-jeremiews-projects.vercel.app",
+      "https://frontend-fruit-shop-w3b3.vercel.app"
     ];
 
     if (allowed.includes(origin)) {
